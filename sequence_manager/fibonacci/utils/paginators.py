d275b4d4c963b4d795777c526dfa90a49eb0d61d
Elmar Aliyev <elmar@aliyevs.com>
2025-04-23 02:26:39 +0200

    Add docstring to the 'FibonacciNumberPagination' class

--- sequence_manager/fibonacci/utils/paginators.py
+++ sequence_manager/fibonacci/utils/paginators.py
@@ -5,9 +5,25 @@ from sequence_manager.utils.custom_responses import JsonResponseError, JsonRespo
 
 
 class FibonacciNumberPagination(PageNumberPagination):
+    """Custom pagination class to enable client-controllable 'page_size' parameter.
+
+    This pagination class allows clients to define the number of results per page
+    by including a 'page_size' query parameter in their requests.
+    """
+
     page_size_query_param = "page_size"
 
     def paginate(self, queryset, request):
+        """Applies pagination to the given queryset and returns a paginated response.
+
+        Args:
+            queryset (QuerySet): The queryset to paginate.
+            request (Request): The incoming request containing pagination parameters.
+
+        Returns:
+            Response: A paginated response containing the data for the current page.
+                      If the page parameters are invalid, returns a JSON error response.
+        """
         try:
             paginated_data = self.paginate_queryset(queryset, request)
             response = self.get_paginated_response(paginated_data)
